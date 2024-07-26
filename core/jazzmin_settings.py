87e944b7bce7db13005356aa2ff907d4d742f1fa
aabboudi <abboudi.abdellah@gmail.com>
2024-07-26 15:55:19 +0100

    Integrated Jazzmin UI for dashboard

---
+++ core/jazzmin_settings.py
@@ -0,0 +1,19 @@
+JAZZMIN_SETTINGS = {
+    "site_title": "MAYA",
+    "site_header": "Admin Dashboard",
+    "site_brand": "Admin Dashboard",
+    "copyright": "Moroccan Association for YES Alumni",
+    "show_ui_builder": False,
+
+    "topmenu_links": [
+        {"model": "auth.User"},
+        {"app": "content"},
+    ],
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar": "navbar-dark",
+    "navbar_fixed": True,
+    "sidebar_nav_small_text": True,
+    "theme": "darkly",
+}
