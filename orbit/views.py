817475ce5ec9187cd09de578e397ef8a363c3791
aabboudi <abboudi.abdellah@gmail.com>
2024-07-26 17:05:56 +0100

    Added orbit app for frontend

---
+++ orbit/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
+
