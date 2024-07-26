18e72c8e1c43610c744227cbd4cc06cdafa4c7a9
aabboudi <abboudi.abdellah@gmail.com>
2024-07-26 17:07:10 +0100

    Added UI tweaks object with examples commented out

--- core/jazzmin_settings.py
+++ core/jazzmin_settings.py
@@ -2,6 +2,7 @@ JAZZMIN_SETTINGS = {
     "site_title": "MAYA",
     "site_header": "Admin Dashboard",
     "site_brand": "Admin Dashboard",
+    "welcome_sign": "Log in to see the admin dashboard.",
     "copyright": "Moroccan Association for YES Alumni",
     "show_ui_builder": False,
 
@@ -12,8 +13,8 @@ JAZZMIN_SETTINGS = {
 }
 
 JAZZMIN_UI_TWEAKS = {
-    "navbar": "navbar-dark",
-    "navbar_fixed": True,
-    "sidebar_nav_small_text": True,
-    "theme": "darkly",
+    # "navbar": "navbar-dark",
+    # "navbar_fixed": True,
+    # "sidebar_nav_small_text": True,
+    # "theme": "darkly",
 }
