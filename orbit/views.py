a8e1d16c580ac1b561ed4dc128f2c9c5c3950785
aabboudi <abboudi.abdellah@gmail.com>
2024-07-29 02:39:45 +0100

    Added pages and improved code

--- orbit/views.py
+++ orbit/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, 'index.html')
+    # return HttpResponse("Hello, world. You're at the polls index.")
 
 def about(request):
     return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def faq(request):
+    return render(request, 'faq.html')
