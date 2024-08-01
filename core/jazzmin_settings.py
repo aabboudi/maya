618e0d714abda21376549592609349d3eee7d94e
aabboudi <abboudi.abdellah@gmail.com>
2024-08-01 15:30:34 +0100

    General code improvements

--- core/jazzmin_settings.py
+++ core/jazzmin_settings.py
@@ -1,15 +1,25 @@
 JAZZMIN_SETTINGS = {
     "site_title": "MAYA",
-    "site_header": "Admin Dashboard",
-    "site_brand": "Admin Dashboard",
+    "site_header": "Moroccan Association for YES Alumni",
+    "site_brand": "Site Administration",
     "welcome_sign": "Log in to see the admin dashboard.",
     "copyright": "Moroccan Association for YES Alumni",
     "show_ui_builder": False,
 
+    "site_logo": "/static/assets/yes-logo-transparent.png",
+    "login_logo": "/static/assets/yes-logo-transparent.png",
+    "site_icon": "/static/assets/favicon.ico",
+
     "topmenu_links": [
+        {"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
         {"model": "auth.User"},
         {"app": "content"},
     ],
+
+    "icons": {
+        "content.post": "fas fa-pen",
+        "content.person": "fas fa-user",
+    },
 }
 
 JAZZMIN_UI_TWEAKS = {
