8df0e824f928c793a15afa669c4bfad07e3b6b7f
aabboudi <abboudi.abdellah@gmail.com>
2024-08-05 03:51:13 +0100

    Updated copy button to display toast notification

--- templates/static/js/main.js
+++ templates/static/js/main.js
@@ -1,8 +1,17 @@
 function copyUrlToClipboard() {
-    const url = window.location.href;
-    navigator.clipboard.writeText(url).then(() => {
-        alert('URL copied to clipboard!');
-    }).catch(err => {
-        alert(`URL could not be copied.\n${err}`);
-    });
+  (async () => {
+    try {
+      await navigator.clipboard.writeText(window.location.href);
+
+      const toast = document.getElementById('dismiss-toast');
+      if (toast) {
+        toast.classList.remove('hidden');
+        setTimeout(() => {
+          toast.classList.add('hidden');
+        }, 5000);
+      }
+    } catch (error) {
+      console.error('Failed to copy URL to clipboard', error);
+    }
+  })();
 }
