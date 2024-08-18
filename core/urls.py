fef3f0c838281f493e7b49aeeb46e4001ab2ff37
aabboudi <abboudi.abdellah@gmail.com>
2024-08-18 02:23:00 +0100

    Optimized suboptimal code

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import handler404
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-handler404 = 'core.views.error_404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orbit.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
