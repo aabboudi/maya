51376dcb1eebd1927fe467fec1cd6b1b2609eb8f
aabboudi <abboudi.abdellah@gmail.com>
2024-08-30 16:24:16 +0100

    Updated Contact page quick links

--- orbit/views.py
+++ orbit/views.py
@@ -112,27 +112,7 @@ def contact(request):
 
         return redirect('/')
 
-    quick_links = [
-        {
-            'title': 'Knowledgebase',
-            'description': 'We\'re here to help with any questions or code.',
-            'cta': 'Contact support',
-            'url': '#'
-        },
-        {
-            'title': 'FAQ',
-            'description': 'Search our FAQ for answers to anything you might ask.',
-            'cta': 'Visit FAQ',
-            'url': '#'
-        },
-        {
-            'title': 'Developer APIs',
-            'description': 'Check out our development quickstart guide.',
-            'cta': 'Contact sales',
-            'url': '#'
-        }
-    ]
-    return render(request, 'contact.html', { 'quick_links': quick_links })
+    return render(request, 'contact.html')
 
 def faq(request):
     faqs = FAQ.objects.all().order_by('-priority', '-created_at')
