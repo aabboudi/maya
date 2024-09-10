b72821c9026ac43ee282f05c2dcccc107428f4a1
aabboudi <abboudi.abdellah@gmail.com>
2024-09-10 17:10:41 +0100

    Updated deployment settings

--- core/dev_settings.py
+++ core/dev_settings.py
@@ -120,6 +120,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ui'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
